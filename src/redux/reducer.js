864012fc336c2a7b70d4962c9656dc13d8ea56c9
owen <owendavies802@gmail.com>
2020-01-11 14:24:44 -0800

    Redux Form Validation

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,7 +3,7 @@ import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 
-const initialState = {
+export const initialState = {
     campsites: CAMPSITES,
     comments: COMMENTS,
     partners: PARTNERS,
